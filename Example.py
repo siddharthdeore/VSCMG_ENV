7f8b48109b859957a0b41faf4f11103947d11386
siddharth deore <siddharthdeore@gmail.com>
2020-02-28 16:45:55 +0100

    Example.py

---
+++ Example.py
@@ -0,0 +1,40 @@
+import numpy as np
+import timeit
+
+# import exposed vscmg envirnment
+import VSCMG_ENV
+
+# create VSCMG satellite object
+sat=VSCMG_ENV.Satellite()
+
+# set satellite states with array of(quaternion, body rates, RW velocities, gimbal angles)
+IC=np.array([1.0, 0.0, 0.0, 0.0,	0.0, 0.0, 0.0,	1000.0, 1000.0, 1000.0, 1000.0,  0.0, 0.0, 0.0, 0.0])
+sat.setState(IC)
+
+# control action array of (4 x RW acerlations, 4 x gimbal rates)
+action=np.array([0.0,0.0,0.0,0.0, 0.0,0.0,0.0,0.0])
+t = 0	# time
+dt = 0	# step size
+
+# take dynamical step of size dt with control action as input
+
+states=sat.action(action,t,dt)
+
+print(state)
+
+# Performance
+stp="""
+import numpy as np
+import timeit
+import VSCMG_ENV
+sat=VSCMG_ENV.Satellite()
+IC=np.array([1.0, 0.0, 0.0, 0.0, 0.0 ,0.0, 0.0, 1000.0, 1000.0, 1000.0, 1000.0,  0.0, 0.0, 0.0, 0.0])
+sat.setState(IC)
+np.set_printoptions(precision=4, suppress=True)
+"""
+stmt="""
+action=np.array([0.0,0.0,0.0,0.0, 0.1,0.1,0.1,0.1])
+sat.step(action,0.0,0.01)
+"""
+N=10000
+print(timeit.timeit(stmt,number=N,setup=stp))
